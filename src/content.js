e88fe57cf816877f131976ec9aba9dbcfec68cbb
Neshad D. Pathirana <112592828+neshaddp@users.noreply.github.com>
2026-02-09 21:11:32 -0500

    Update content.js

--- src/content.js
+++ src/content.js
@@ -44,6 +44,11 @@ export const affiliationLogos = [
 // You can update URLs later here only.
 export const socialLinks = [
   {
+    label: "CV",
+    href: "/Neshad_CV.pdf",
+    icon: "cv"
+  },
+  {
     label: "LinkedIn",
     href: "https://www.linkedin.com/in/neshad1996?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
     icon: "linkedin"
